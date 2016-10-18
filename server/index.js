2dd3b24a366010c77e7fe2363bbc355597d16c49
Jared Grippe <jared@deadlyicon.com>
2016-10-18 10:26:07 -0700

    added general error handlers and bodyParser

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import path from 'path'
 import express from 'express'
+import bodyParser from 'body-parser'
+import errorHandlers from './errorHandlers'
 import apiRoutes from './api'
 
 const publicPath = path.resolve(__dirname, '../public')
@@ -9,12 +11,15 @@ server.set('env', process.env.NODE_ENV)
 server.set('port', process.env.PORT || '3000')
 
 server.use(express.static(publicPath))
+server.use(bodyParser.json())
 
 server.use('/api', apiRoutes)
 
 server.get('/*', (request, response) => {
   response.sendFile(publicPath+'/index.html')
 });
+
+server.use(errorHandlers)
 
 if (process.env.NODE_ENV !== 'test'){
   console.log('http://localhost:'+server.get('port')+'/')
